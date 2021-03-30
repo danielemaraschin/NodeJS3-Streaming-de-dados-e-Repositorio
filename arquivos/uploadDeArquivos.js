b903bb8ee63dd0ccffe634a0feb68874ae3ac47c
danielemaraschin <danikmaraschin@gmail.com>
2021-03-30 08:44:24 +0200

    working with .pipe and createWriteStream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
 
 fs.createReadStream('../assets/salsicha.jpg') //leitura da imagem
+    .pipe(fs.createWriteStream('../assets/salsicha-stream.jpg'))//escrita da imagem
     
