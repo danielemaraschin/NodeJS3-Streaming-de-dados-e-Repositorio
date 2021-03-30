de511ced3d1cce73d2dafc1fc2de2f78d5a5e434
danielemaraschin <danikmaraschin@gmail.com>
2021-03-30 08:41:42 +0200

    working with createReadStream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,4 @@
 const fs = require('fs')
-/*1ro parametro é o caminho e o 2do pode ser omitido, da algumas config, 3ro funcao callback
-a funcao recebe 1ro o erro e 2do a imagem  (buffer de memoria que a gente salvou) como param*/
-fs.readFile('../assets/salsicha.jpg', (erro, buffer) => {
-    console.log("imagem bufferizada")
+
+fs.createReadStream('../assets/salsicha.jpg') //leitura da imagem
     
-    fs.writeFile('./assets.salsicha2.jpg', buffer, erro => {
-        console.log('imagem foi escrita')
-    })
-})
