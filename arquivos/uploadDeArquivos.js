d331a893e9bbb0dcdab5f31db0065dc81da9c670
danielemaraschin <danikmaraschin@gmail.com>
2021-03-30 11:02:08 +0200

    working with .on

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -2,4 +2,5 @@ const fs = require('fs')
 
 fs.createReadStream('../assets/salsicha.jpg') //leitura da imagem
     .pipe(fs.createWriteStream('../assets/salsicha-stream.jpg'))//escrita da imagem
+    .on('finish', () => console.log('imagem escrita com sucesso')) //chama evento e avisa que foi feito
     
