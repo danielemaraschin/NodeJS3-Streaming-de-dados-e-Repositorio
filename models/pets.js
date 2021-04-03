d3149f272c1363f29a5d25bafed6b26b86dfc14d
danielemaraschin <danikmaraschin@gmail.com>
2021-04-03 18:50:01 +0200

    image address on variable

--- models/pets.js
+++ models/pets.js
@@ -4,13 +4,17 @@ const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 class Pet {
     adiciona(pet, res) {
         const query = 'INSERT INTO Pets SET ?'
-        
-        conexao.query(query, pet, erro => { 
-            if(erro) {
-                res.status(400).json(erro)
-            }else {
-               res.status(200).json(pet)
-            }
+        //caminho da imagem, nome do pet = do arquivo, funcao callBack
+        uploadDeArquivo(pet.imagem, pet.nome, (novoCaminho) => {
+            const novoPet = { nome: pet.nome, imagem: novoCaminho}
+
+            conexao.query(query, novoPet, erro => { 
+                if(erro) {
+                    res.status(400).json(erro)
+                }else {
+                res.status(200).json(pet)
+                }
+            })
         })
     }
 }
