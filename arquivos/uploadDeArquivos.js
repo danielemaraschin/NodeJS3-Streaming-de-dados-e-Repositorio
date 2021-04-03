22713644c086bd4dda496179bedc57afc50e984e
danielemaraschin <danikmaraschin@gmail.com>
2021-04-03 18:26:19 +0200

    saving the image address on variable

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 
 module.exports = (caminho, nomeDoArquivo,callbackImagemCriada) => {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
     fs.createReadStream(caminho) //leitura da imagem
-    .pipe(fs.createWriteStream(`./assets/imagens/${nomeDoArquivo}`))//escrita da imagem
-    .on('finish', () => callbackImagemCriada()) //chama evento e avisa que foi feito
+    .pipe(fs.createWriteStream(novoCaminho))//escrita da imagem
+    .on('finish', () => callbackImagemCriada(novoCaminho)) //chama evento e avisa que foi feito
 }
 
     
