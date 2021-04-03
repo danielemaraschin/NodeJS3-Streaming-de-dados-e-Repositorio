9e3aec92439a3eeefdd0497d4bb56aa2738bcd35
danielemaraschin <danikmaraschin@gmail.com>
2021-04-03 18:38:22 +0200

    format document

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-module.exports = (caminho, nomeDoArquivo,callbackImagemCriada) => {
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
     const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+
     fs.createReadStream(caminho) //leitura da imagem
-    .pipe(fs.createWriteStream(novoCaminho))//escrita da imagem
-    .on('finish', () => callbackImagemCriada(novoCaminho)) //chama evento e avisa que foi feito
+        .pipe(fs.createWriteStream(novoCaminho))//escrita da imagem
+        .on('finish', () => callbackImagemCriada(novoCaminho)) //chama evento e avisa que foi feito
 }
 
-    
