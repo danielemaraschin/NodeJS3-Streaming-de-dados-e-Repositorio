dd6edd4dbad5c2c1d41b2bb9d251855679b2b8de
danielemaraschin <danikmaraschin@gmail.com>
2021-04-04 14:05:47 +0200

    working with path.extname( )

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -3,11 +3,20 @@ const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
 
+    const tiposValidos = ['jpg', 'png', 'jpeg']
     const tipo = path.extname(caminho)
-    const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1))
+    //pergunta qual o index do tiposvalidos. O ponto final é o primeiro entao coloca tipo.substring
+    // pra contar a partir do 1 e nao contar o ponto final antes da extensao
+    //se o valor for -1 é que a extensao nao está dentro do array de tiposValidos
 
-    fs.createReadStream(caminho) //leitura da imagem
-        .pipe(fs.createWriteStream(novoCaminho))//escrita da imagem
-        .on('finish', () => callbackImagemCriada(novoCaminho)) //chama evento e avisa que foi feito
+    if (tipoEhValido === -1) {
+        console.log('Erro! Tipo de imagem inválida.')
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+        fs.createReadStream(caminho) //leitura da imagem
+            .pipe(fs.createWriteStream(novoCaminho))//escrita da imagem
+            .on('finish', () => callbackImagemCriada(novoCaminho)) //chama evento e avisa que foi feito
+    }
 }
-
