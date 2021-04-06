d758ecc91b5e62596d4f60b1ab0a8a16ed37dd11
danielemaraschin <danikmaraschin@gmail.com>
2021-04-06 09:42:44 +0200

    working with external API (CPF)

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -6,8 +6,8 @@ class Tabelas {
         this.criarPets()        //já tá chamando essas funções
     }
 
-    criarAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+    criarAtendimentos() {                                        //cliente varchar 11 digitos pq será identificado pelo CPF
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             if (erro) {
